74c2f99c5e45b96860e47c2e283b46d7f5ebfa7a
iopopoi <kimkin0668@gmail.com>
2022-10-16 21:50:29 +0900

    ADD: start spring project

---
+++ diary/src/main/java/com/uhihi/diary/UhihiDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.uhihi.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UhihiDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UhihiDiaryApplication.class, args);
+	}
+
+}
